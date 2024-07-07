467c0f2f55daf5e99c6f3006a81c47c14d713d4a
Antonis <maglaras@gmail.com>
2024-07-07 23:30:02 +0300

    Create index.php
    index.php
    for displaying the graphs

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<HTML>
+<TITLE>:: Particle Matter Sensor ::</TITLE>
+<BODY>
+
+
+<STYLE TYPE="text/css">
+
+html {
+
+.imageContainer > img:hover {
+  width: 100%;
+  height: 100%;
+}
+}
+</STYLE>
+<center>
+<table width=auto height=100%> 
+    <tr>
+    <tr>
+        <td style="text-align: center; vertical-align: middle;">
+            <img src="pmlast24h.php" border=0>
+        </td>
+    </tr>
+    <tr><td><br></td></tr>
+    <tr>
+        <td style="text-align: center; vertical-align: middle;">
+            <img src="particleslast24h.php" border=0>            
+        </td>
+    </tr>
+    <tr><td><br></td></tr>
+    <tr>
+        <td style="text-align: center; vertical-align: middle;">
+            <img src="batterylast24h.php" border=0>
+        </td>
+    </tr>
+</table>
+</center>
+</BODY>
+</HTML>
